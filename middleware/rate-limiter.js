d552789f1313c795c8ebb2dbb9979013db3d8d06
menashekoren <menashekoren@gmail.com>
2022-03-29 19:42:55 +0300

    Hotfix: Update rate limiter

--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 module.exports.limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+  windowMs: 5 * 60 * 1000,
+  max: 1500,
   standardHeaders: false,
   legacyHeaders: false,
 });
